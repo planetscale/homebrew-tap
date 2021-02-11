fb729668833d385dba4b75c472f539e9b9cb156d
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-11 06:06:49 -0800

    Brew formula update for go-releaser-test version v0.1.18

--- Formula/pscale.rb
+++ Formula/pscale.rb
@@ -6,21 +6,21 @@ require_relative "../custom_download_strategy"
 class Pscale < Formula
   desc "The PlanetScale CLI"
   homepage "https://planetscale.com/"
-  version "0.1.17"
+  version "0.1.18"
   license "Apache 2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/planetscale/go-releaser-test/releases/download/v0.1.17/go-releaser-test_0.1.17_macOS_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "d3151ab174fec55f43dafa6dfef19d8ef1e4101140ca1c653cb7c1d1a6e9cc9f"
+    url "https://github.com/planetscale/go-releaser-test/releases/download/v0.1.18/go-releaser-test_0.1.18_macOS_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "61b430e8570921543386774b606a9820d3c9a1462d8b082d32e23f49aae0f28a"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/planetscale/go-releaser-test/releases/download/v0.1.17/go-releaser-test_0.1.17_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "9ef089fdc9d3f44dc3ed057b9248156dec714f1c1a1f8f8ebe5868a37a75ab3a"
+    url "https://github.com/planetscale/go-releaser-test/releases/download/v0.1.18/go-releaser-test_0.1.18_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "d43718220c3e6e9fa86bfd730284974ddd0a405b1054c2787728a5457dd5d3f3"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/planetscale/go-releaser-test/releases/download/v0.1.17/go-releaser-test_0.1.17_linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "df652167f59c990ab3a5cdfcd1ababfccc1bf58785cba0baaaa457eda1170ccf"
+    url "https://github.com/planetscale/go-releaser-test/releases/download/v0.1.18/go-releaser-test_0.1.18_linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "66febdd65d37524f1d0348ac0e493ff89cbb88d93d23dedcd102887a3123b836"
   end
 
   depends_on "mysql" => :optional
