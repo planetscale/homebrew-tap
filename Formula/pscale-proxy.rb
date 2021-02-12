660b5847ede183831e16f30a3581f1050b8e721e
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-12 11:59:54 -0800

    Brew formula update for pscale-proxy version v0.1.2

--- Formula/pscale-proxy.rb
+++ Formula/pscale-proxy.rb
@@ -6,21 +6,21 @@ require_relative "../custom_download_strategy"
 class PscaleProxy < Formula
   desc "The PlanetScale SQL Proxy"
   homepage "https://planetscale.com/"
-  version "0.1.1"
+  version "0.1.2"
   license "Apache 2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/planetscale/sql-proxy/releases/download/v0.1.1/pscale-proxy_0.1.1_macOS_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "aba228fed2d4b0a00916361c0b216c807c7e1a5018d68b2c8e29c4d3922a2fd8"
+    url "https://github.com/planetscale/sql-proxy/releases/download/v0.1.2/pscale-proxy_0.1.2_macOS_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "ea43eb66f9bdc107d147026f947117605cd5298e6ac7c153fa40a661bd677f79"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/planetscale/sql-proxy/releases/download/v0.1.1/pscale-proxy_0.1.1_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "59e6cc18f9bdd8c324966408d366c11d500bfdf10411cc62f69877bc899fd4a6"
+    url "https://github.com/planetscale/sql-proxy/releases/download/v0.1.2/pscale-proxy_0.1.2_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "5c8b1fbf4aa361fc76397b9613f4e2ce123fc8ba6eac23a15c39a08185a4c9af"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/planetscale/sql-proxy/releases/download/v0.1.1/pscale-proxy_0.1.1_linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "944dbd3e628916d9044a138c4de82dc55bef47598a7b9e75ed7d38094651e664"
+    url "https://github.com/planetscale/sql-proxy/releases/download/v0.1.2/pscale-proxy_0.1.2_linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "b042c1a38adcb40eab184bac31a2b21d86b13bb95c26451e1ec6a5e3637acbed"
   end
 
   def install
