417e5eeb7a8c4f81d568089c69ea799a3f1a6294
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-08 11:26:58 -0400

    Brew formula update for pscale version v0.70.0

--- Formula/pscale.rb
+++ Formula/pscale.rb
@@ -5,29 +5,29 @@
 class Pscale < Formula
   desc "The PlanetScale CLI"
   homepage "https://planetscale.com/"
-  version "0.69.0"
+  version "0.70.0"
   license "Apache 2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/planetscale/cli/releases/download/v0.69.0/pscale_0.69.0_macOS_amd64.tar.gz"
-      sha256 "3bef3d96b0a110d0b775a531dcfe18f1eb00f195056234320274b6aa67198eba"
+      url "https://github.com/planetscale/cli/releases/download/v0.70.0/pscale_0.70.0_macOS_amd64.tar.gz"
+      sha256 "e022fb3682e040b101a9da1a861516121249a948ca96bcf2f6c517393bfba2f2"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/planetscale/cli/releases/download/v0.69.0/pscale_0.69.0_macOS_arm64.tar.gz"
-      sha256 "657feb72946259514682b43aac8437912c397c466d69784bb5812a0444967ba5"
+      url "https://github.com/planetscale/cli/releases/download/v0.70.0/pscale_0.70.0_macOS_arm64.tar.gz"
+      sha256 "0b67cdba9a03e8fdc57e0c5a0291526c75a6850cea9455bb7b47ad6c066bf81c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/planetscale/cli/releases/download/v0.69.0/pscale_0.69.0_linux_amd64.tar.gz"
-      sha256 "6f51964089e001226d61e7923936e04e9c021ba049ddaa64703f76b66423ae44"
+      url "https://github.com/planetscale/cli/releases/download/v0.70.0/pscale_0.70.0_linux_amd64.tar.gz"
+      sha256 "39fe9f8d76ea992f46e15611d07b5d6b96c887fbc8c3f96f301b126c65e3f946"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/planetscale/cli/releases/download/v0.69.0/pscale_0.69.0_linux_arm64.tar.gz"
-      sha256 "7d8ffd580cadbb2159af03825ce33beabada95424210e4450cd13202942347a0"
+      url "https://github.com/planetscale/cli/releases/download/v0.70.0/pscale_0.70.0_linux_arm64.tar.gz"
+      sha256 "061c17abc31b8a3b205073efa2ae4b26eeac74b1e6eddcb2cceacebbd5b8e762"
     end
   end
 
