b12c13211fd5ff26dcce55c217c6448668acca24
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-10 03:02:34 -0400

    Brew formula update for pscale version v0.71.0

--- Formula/pscale.rb
+++ Formula/pscale.rb
@@ -5,29 +5,25 @@
 class Pscale < Formula
   desc "The PlanetScale CLI"
   homepage "https://planetscale.com/"
-  version "0.70.0"
+  version "0.71.0"
   license "Apache 2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/planetscale/cli/releases/download/v0.70.0/pscale_0.70.0_macOS_amd64.tar.gz"
-      sha256 "e022fb3682e040b101a9da1a861516121249a948ca96bcf2f6c517393bfba2f2"
-    end
-    if Hardware::CPU.arm?
-      url "https://github.com/planetscale/cli/releases/download/v0.70.0/pscale_0.70.0_macOS_arm64.tar.gz"
-      sha256 "0b67cdba9a03e8fdc57e0c5a0291526c75a6850cea9455bb7b47ad6c066bf81c"
+      url "https://github.com/planetscale/cli/releases/download/v0.71.0/pscale_0.71.0_macOS_amd64.tar.gz"
+      sha256 "45dbe9804e11d65539feb1f6370c1da7421e3aee5b29cea617c8aecc6d388913"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/planetscale/cli/releases/download/v0.70.0/pscale_0.70.0_linux_amd64.tar.gz"
-      sha256 "39fe9f8d76ea992f46e15611d07b5d6b96c887fbc8c3f96f301b126c65e3f946"
+      url "https://github.com/planetscale/cli/releases/download/v0.71.0/pscale_0.71.0_linux_amd64.tar.gz"
+      sha256 "23e7670bf5947b4efd308bcd7f6a466ac5d8e7c1cb8e4740b1a6c672e55f5738"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/planetscale/cli/releases/download/v0.70.0/pscale_0.70.0_linux_arm64.tar.gz"
-      sha256 "061c17abc31b8a3b205073efa2ae4b26eeac74b1e6eddcb2cceacebbd5b8e762"
+      url "https://github.com/planetscale/cli/releases/download/v0.71.0/pscale_0.71.0_linux_arm64.tar.gz"
+      sha256 "07d22fdc1bf348bfdb6862ddfe82a66cd1c8f9f186cab5805d2e863e7d04a8cb"
     end
   end
 
