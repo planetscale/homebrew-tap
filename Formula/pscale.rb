c7603a37d934abc6fe658c431f1100f684b1fa3d
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-28 12:49:22 -0400

    Brew formula update for pscale version v0.79.0

--- Formula/pscale.rb
+++ Formula/pscale.rb
@@ -5,29 +5,29 @@
 class Pscale < Formula
   desc "The PlanetScale CLI"
   homepage "https://planetscale.com/"
-  version "0.78.0"
+  version "0.79.0"
   license "Apache 2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/planetscale/cli/releases/download/v0.78.0/pscale_0.78.0_macOS_amd64.tar.gz"
-      sha256 "22c7c70117d315715a6de693f0f9ddc317c41fec6381f0c3c8bb9f0b041565f5"
+      url "https://github.com/planetscale/cli/releases/download/v0.79.0/pscale_0.79.0_macOS_amd64.tar.gz"
+      sha256 "f9f24bd4b661ba07277cc290a568fb89c8e6ad328875e2b0334638e8832bc703"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/planetscale/cli/releases/download/v0.78.0/pscale_0.78.0_macOS_arm64.tar.gz"
-      sha256 "3df321e6a09195755e795a986bb099453793b2c5b3e117f445957ffdc82efdcb"
+      url "https://github.com/planetscale/cli/releases/download/v0.79.0/pscale_0.79.0_macOS_arm64.tar.gz"
+      sha256 "9d831c0157248017b66031992f5ee76f0c49e8f85c696f7c5d6a01b01118d528"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/planetscale/cli/releases/download/v0.78.0/pscale_0.78.0_linux_amd64.tar.gz"
-      sha256 "5cd4298f2c5ecab97d875c7fe69db741746a842757ed8cfc84f728a5afe6bfaa"
+      url "https://github.com/planetscale/cli/releases/download/v0.79.0/pscale_0.79.0_linux_amd64.tar.gz"
+      sha256 "9185aabe4f42a233d9c33bad240939f3d465fb28db23840431ec9a2a629cd5f9"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/planetscale/cli/releases/download/v0.78.0/pscale_0.78.0_linux_arm64.tar.gz"
-      sha256 "c1ca90009d339667bf5123248c18d12c93fcb6e8f7c267b4a71fdd1e2397c841"
+      url "https://github.com/planetscale/cli/releases/download/v0.79.0/pscale_0.79.0_linux_arm64.tar.gz"
+      sha256 "5c1686247b326d3c0f9d28176a159e993cd5474b080f99058f9e6e74fc7c1e78"
     end
   end
 
